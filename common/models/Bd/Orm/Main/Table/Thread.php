f906047163a55080b6c1ec0633a4b2bef9ef9938
tomita <tomita@sunrisedigital.jp>
2013-10-17 13:18:13 +0900

    fetchAllNewOrdered

--- common/models/Bd/Orm/Main/Table/Thread.php
+++ common/models/Bd/Orm/Main/Table/Thread.php
@@ -4,7 +4,18 @@ require_once 'Bd/Orm/Main/Base/Table/Thread.php';
 
 class Bd_Orm_Main_Table_Thread extends Bd_Orm_Main_Base_Table_Thread
 {
-
+    public function fetchAllNewerOrdered(Sdx_Db_Select $select = null)
+    {
+        if($select === null)
+            {
+            $select = $this->getSelect();
+        }
+        
+        $select->order('created_at DESC');
+        
+        return $this->fetchAll($select);
+    }
+        
 
 }
 
