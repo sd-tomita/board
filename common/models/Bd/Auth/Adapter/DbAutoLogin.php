3567c90a5381f4c09d9836bff1e9962cdb9d3a8d
tomita <tomita@sunrisedigital.jp>
2013-10-18 18:10:55 +0900

    DbAutoLogin.php edit

--- common/models/Bd/Auth/Adapter/DbAutoLogin.php
+++ common/models/Bd/Auth/Adapter/DbAutoLogin.php
@@ -4,12 +4,11 @@
  **/
 class Bd_Auth_Adapter_DbAutoLogin extends Sdx_Auth_Adapter_Db2
 {
-
   /**
    * Cookieに保存したユニークなトークン
    */
   private $_token;
-
+ 
   /**
    * @param string $token Cookieに保存したユニークなトークン
    */
@@ -17,34 +16,35 @@ class Bd_Auth_Adapter_DbAutoLogin extends Sdx_Auth_Adapter_Db2
   {
     $this->_token = $token;
   }
-
+ 
   /**
    * アカウントのデータを検索して返す
    * @return boolean|Bd_Orm_Main_AutoLogin
    */
   protected function _find()
   {
-      $t_auto_login = Bd_Orm_Main_AutoLogin::getTable();
-      $auto_login = $t_auto_login->findByPkey($this->_token);
-      if (!$auto_login instanceof Bd_Orm_Main_AutoLogin)
-      {
-          return false;
-      }
-      return $auto_login;
+    $t_auto_login = Bd_Orm_Main_AutoLogin::getTable();
+    $auto_login = $t_auto_login->findByPkey($this->_token);
+    if (!$auto_login instanceof Bd_Orm_Main_AutoLogin)
+    {
+      return false;
+    }
+ 
+    return $auto_login;
   }
-
+ 
   /**
    * パスワードが一致しているか検証する
-   * @param mixed $account
-   * @return bool|array セッションの保持されてSdx_Userから取得可能になります。例：array('login_id'=>'xxxx', 'role'=>'xxx')
+   * @param Bd_Orm_Main_AutoLogin $account
+   * @return bool|array array('login_id'=>'xxxx', 'role'=>'xxx')
    */
   protected function _auth($auto_login)
   {
-      $account = $auto_login->getAccount();
-      
-      return array(
-          'id' => $account->getId(),
-          'login_id' => $account->getLoginId(),
-      );
+    $account = $auto_login->getAccount();
+ 
+    return array(
+      'id' => $account->getId(),
+      'login_id' => $account->getLoginId(),
+    );
   }
 }
