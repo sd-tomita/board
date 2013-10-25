8303a1d4d24d1029856f00808ea4d58851e10b74
tomita <tomita@sunrisedigital.jp>
2013-10-25 19:45:39 +0900

    差分2

--- board.sunrisedigital.jp/app/controllers/default/IndexController.php
+++ board.sunrisedigital.jp/app/controllers/default/IndexController.php
@@ -17,9 +17,9 @@ class IndexController extends Sdx_Controller_Action_Http
             //threadテーブルクラスの取得
             $t_thread = Bd_Orm_Main_Thread::createTable();
             //データを取得。DBへの接続トランザクションは見るだけなので必要なし。
-            $thread = $t_thread->getSelect();
+            $thread = $t_thread->fethcAll();
             //結果を出力
-            Sdx_Debug::dump($t_thread, 'Pkey');
+            Sdx_Debug::dump($thread, 'Pkey');
         }
         
 }
