a5fcdc29e8a6399759a3e5c6b63722f6115683ec
tomita <tomita@sunrisedigital.jp>
2013-11-05 18:55:32 +0900

    保存を別アクションに変更し、その上で正常に動作するようコードを修正しました

--- board.sunrisedigital.jp/app/controllers/default/ThreadController.php
+++ board.sunrisedigital.jp/app/controllers/default/ThreadController.php
@@ -18,7 +18,7 @@ class ThreadController extends Sdx_Controller_Action_Http
     {
         //URLからthread_idを取得。
         //なお、thread_idはroute.ymlに付けた変数名。
-        $num = $this->_getParam('thread_id');
+        //$num = $this->_getParam('thread_id');変数を使う必要なくなったのでコメントアウト
         //entryテーブルクラスの取得
         $t_entry = Bd_Orm_Main_Entry::createTable();
         //JOIN予定のAccountテーブルのテーブルクラスを取得
@@ -32,24 +32,36 @@ class ThreadController extends Sdx_Controller_Action_Http
         //ORDER BY thread_id ASC(thread_idを昇順でSELECT)
         $select->order('thread_id ASC');
         //URLから受け取ったID分だけをSelectするよう条件追記
-        $select->where("thread.id = $num");
+        $select->add("thread.id", $this->_getParam('thread_id'));
          //fetchAll()で全て取得して$entryへ入れておく
         $entry = $t_entry->fetchAll($select);
         //$entryの内容をテンプレに渡す。
         $this->view->assign("entry_list", $entry);
         
         //確認用ダンプ出力。いらなくなったら消す
-        Sdx_Debug::dump($entry, '$entryの出力結果');
+        //Sdx_Debug::dump($entry, '$entryの出力結果');
+        Sdx_Debug::dump($_SESSION, '$_SESSIONの出力結果');
         
         //コメント投稿関係はこっちのメソッドに任せる。
-        $this->formCreation();
+        $form = $this->formCreation();
+        Sdx_Debug::dump($form, '$formの出力結果');
+
+        if(isset($_SESSION['form']))
+        {
+          $form->bind($_SESSION['form']);
+          $form->execValidate();
+          unset($_SESSION['form']);
+        }
+        $this->view->assign('form', $form);
+        
+        
 
     }
     private function formCreation()
     {
         $form = new Sdx_Form();//インスタンス作成
         $form
-        ->setActionCurrentPage() //アクション先をこのページに設定
+        ->setAction('/thread/'.$this->_getParam('thread_id').'/save-entry') //アクション先を設定
         ->setMethodToPost();     //メソッドをポストに変更
  
         //各エレメントをフォームにセット
@@ -57,7 +69,7 @@ class ThreadController extends Sdx_Controller_Action_Http
         $elem = new Sdx_Form_Element_Text();
         $elem
                 ->setName('account_id')
-                ->addValidator(new Sdx_Validate_NotEmpty('なんか数字を入力してくれよ'))
+                ->addValidator(new Sdx_Validate_NotEmpty('何も入力ないのは寂しいです'))
                 ->addValidator(new Sdx_Validate_Regexp('/^[0-9]+$/u','つかえるのは数字だけね'));
         $form->setElement($elem);
         //コメント
@@ -65,45 +77,61 @@ class ThreadController extends Sdx_Controller_Action_Http
         $elem
                 ->setName('body')
                 //とりあえずコメントだけなのでNULL値チェックだけでよいかと。
-                ->addValidator(new Sdx_Validate_NotEmpty('なんか入力してくれよ'));
+                ->addValidator(new Sdx_Validate_NotEmpty('何も入力ないのは寂しいです'));
         $form->setElement($elem);
-        
-        //formがsubmitされていたら
+       
+        return $form;
+    }
+    public function saveEntryAction()
+    {
+        //submitが押されていれば
         if($this->_getParam('submit'))
         {
+          $form = $this->formCreation();
           //Validateを実行するためにformに値をセット
           //エラーが有った時各エレメントに値を戻す処理も兼ねてます
           $form->bind($this->_getAllParams());//bindメソッドは主に取得したパラメータを配列にしてセット
           
-          $entry = new Bd_Orm_Main_Entry();//データベース入出力関係のクラスはこっちにある。
-          $db = $entry->updateConnection();
           
-          $db->beginTransaction();
-          try
-          {
+            $entry = new Bd_Orm_Main_Entry();//データベース入出力関係のクラスはこっちにある。
+            $db = $entry->updateConnection();
+                  
+            $db->beginTransaction();
+                  
+            try
+            {
+              session_start();
               if($form->execValidate())
               {
-                  $entry
-                          ->setBody($this->_getParam('body'))
-                          ->setThreadId($this->_getParam('thread_id'))
-                          ->setAccountId($this->_getParam('account_id'));
-                  $entry->save();
-                  $db->commit();
-                  $th_id = $this->_getParam('thread_id');//リダイレクト時のURLに使う
-                  $this->redirectAfterSave("thread/$th_id/list");
+                $entry
+                  ->setBody($this->_getParam('body'))
+                  ->setThreadId($this->_getParam('thread_id'))
+                  ->setAccountId($this->_getParam('account_id'));
+                $entry->save();
+                $db->commit();
+                $this->redirectAfterSave("thread/{$this->_getParam('thread_id')}/list");
               }
               else
               {
                   $db->rollback();
+                  $_SESSION['form'] = $this->_getAllParams();
+                  $this->redirectAfterSave("thread/{$this->_getParam('thread_id')}/list#entry-form");
               }
-          }
-          catch (Exception $e)
-          {
+            
+            }
+            catch (Exception $e)
+            {
               $db->rollBack();
               throw $e;
+            }
           }
-        }
-         $this->view->assign('form', $form);
+          else
+          {
+            $this->redirectAfterSave("thread/{$this->_getParam('thread_id')}/list#entry-form");
+          }
+         
     }
+        
+    
 }
 ?>
