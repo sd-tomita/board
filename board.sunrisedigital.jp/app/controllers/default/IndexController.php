57e857de5a7d6e89463a9da9843700ed7b1f299f
tomita <tomita@sunrisedigital.jp>
2013-11-08 17:29:03 +0900

    サブクエリをSdx_Db_Selectで生成する方法に変更しました

--- board.sunrisedigital.jp/app/controllers/default/IndexController.php
+++ board.sunrisedigital.jp/app/controllers/default/IndexController.php
@@ -18,20 +18,45 @@ class IndexController extends Sdx_Controller_Action_Http
             
             //inner join
             //$t_thread->addJoinInner($t_entry);
+            
+            //ネストする用のSelect
+            //　↓これを Sdx_Db_Select でつくりたい！
+              //(SELECT thread_id, MAX(entry.updated_at) AS newest_date FROM entry GROUP BY thread_id) AS sub
+            $sub_sel = $t_entry->getSelect();
+            $sub_sel->group('thread_id');
 
-            //selectの作成
+            $sub_sel->resetColumns();//getSelect()でデフォルトで設定されているSELECTの対象「*」をリセット
+            $sub_sel->columns(array(
+                0 => 'thread_id',//連想配列で統一するため敢えてキー｢0｣を付けているだけ。 
+                'newest_date' => 'MAX(entry.created_at)'
+            ));
+            Sdx_Debug::dump($sub_sel->assemble(), '$sub_selのSQL');
+  
+
+            //本selectの作成
             $select = $t_thread->getSelectWithJoin();
             
             //まずグループ化。
-            $select->group('thread_id');
+            //$select->group('thread_id');　必要ないのでひとまずコメント化。
             
-            //書き方がわからなかったのでSQLインジェクション対策は一旦置いてます。
-            $select->joinLeft('entry as sub', 'thread.id = sub.thread_id', 'thread.id,  max(sub.created_at) as newest_date');
+            //ネストされるSQL文の外側を()で囲うためだけのものです。
+            $sub = sprintf('(%s)', $sub_sel->assemble());
+            //本Selectにjoinする。
+            $select->joinLeft(
+                  array('sub' => new Zend_Db_Expr($sub)) ,
+                  'thread.id = sub.thread_id'
+            );
+            
+            //SQL文を直接挿入する書き方。Sdx_Db_Select使用のため一旦コメント化。
+//            $select->joinLeft(
+//                    array('sub' => new Zend_Db_Expr('(select thread_id, max(entry.created_at) as newest_date from entry group by thread_id)')), 
+//                    'thread.id = sub.thread_id'                    
+//            );
             
             $select->order('newest_date DESC');
             $thread = $t_thread->fetchAll($select);
-         
-            Sdx_Debug::dump($select->assemble(), 'dump');//assembleでSelect結果を配列化
+                    
+            Sdx_Debug::dump($select->assemble(), '$selectのSQL');//assembleでSelect結果を配列化
             
             //テンプレで使えるように$threadの内容をテンプレにアサインする。
             $this->view->assign("thread_list", $thread);
