00d367f075511b9b8f7cfe3469b756a6ca0e5279
tomita <tomita@sunrisedigital.jp>
2013-12-12 15:01:40 +0900

    Ajax通信時の負荷軽減のため、通信中はsubmitにロックがかかるようにしました。
    通信中もsubmitボタンが連打できる状態だったので、そのあたりを改善しています。

--- board.sunrisedigital.jp/web/js/search.js
+++ board.sunrisedigital.jp/web/js/search.js
@@ -9,10 +9,11 @@
 //    alert("jqueryの読み込みが完了しました");
 //  });
 
-
 //サーバー通信
 $(function(){
-  $("#search-form input[type='submit']").click(function(){
+  $("#search-form input[type='submit']").on('click', function(){
+    //通信終わるまで送信ボタン無効化
+    $("#search-form input[type='submit']").attr('value', 'please wait...').attr('disabled', true);
     /* *
      * フォームの値を取得、Ajax送信まで。
      */
@@ -20,17 +21,20 @@ $(function(){
     //→こうなるはず　genre_id=○○&tag_id[△△]=△△
     var $form = $("#search-form");
     var query = $form.serialize();
-      
     $.ajax({
       type: "GET",
       url: "/thread/entrance/thread-list", 
       data: query,
     }).done(function(data){
         $('.thread_list').remove();//古いdataの削除
+        $('input[name="more"]').remove();
         $('.data-disp').append(data);
+        $('.data-disp').append('<input type=button name="more" value="more">');
     }).fail(function(data){
           alert("ng");
-    });          
+    }).always(function(data){
+        //.complete()の代わり。ここでボタンを元に戻す
+        $("#search-form input[type='submit']").attr('value', '検索開始').attr('disabled', false);        
+    });
   });
 });
-
